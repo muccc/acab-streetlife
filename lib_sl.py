b0450876840c49937d8546092298191a97e47fe7
Kilian Neuner <kili@arkadin.club.muc.ccc.de>
2011-06-02 17:29:40 +0200

    move basic functions to lib_sl.py

---
+++ lib_sl.py
@@ -0,0 +1,22 @@
+import urllib2
+
+HOST="127.1"
+PORT=8080
+
+def send(x,y,r,g,b,t=0):
+  message = "/%s/%s/%i/%i/%i/%i" % (x, y, r, b, g, t*1000)
+  urllib2.urlopen("http://%s:%i"% (HOST, PORT)+message)
+
+def matrix(targetsize_x=8,targetsize_y=6):
+  x=0
+  y=0
+  matrix = []
+  while ( y < targetsize_y):
+    while ( x < targetsize_x):
+      p =  str(x) + str(y)
+      matrix.append(p)
+      x = x + 1
+
+    y = y + 1
+    x = 0
+  return matrix
