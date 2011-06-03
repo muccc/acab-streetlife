021ef13b41e1e37a5b5d323fb6fa349b63b60172
Kilian Neuner <kili@arkadin.club.muc.ccc.de>
2011-06-04 00:21:31 +0200

    add scheduler.py

---
+++ scheduler.py
@@ -0,0 +1,34 @@
+import subprocess
+import playlist
+import time
+import os
+import signal
+
+pidfile='/tmp/animation_pids'
+
+pl=playlist.playlist
+
+def abort(signal,frame,pids):
+  for pid in pids:
+   os.kill(pid,signal.SIGKILL)
+
+signal.signal(signal.SIGINT, abort)
+signal.pause
+
+while 1:
+ for i in pl:
+  t = i[0]
+  processlist=[]
+  pids=open(pidfile,"w")
+  pids.close()
+  for animation in i[1]:
+    p=subprocess.Popen(["python", animation])
+    processlist.append(p)
+    print p.pid
+    pids= open(pidfile,'a')
+    pids.write(str(p.pid)+"\n")
+    pids.close()
+  time.sleep(t)
+  for process in processlist:
+    process.kill()
+  print "next entry"
