eee1b198e7a699abb5593bb5f703f242d1a41573
Kilian Neuner <kili@arkadin.club.muc.ccc.de>
2011-06-03 21:35:46 +0200

    add randomfade2.py

---
+++ randomfade2.py
@@ -0,0 +1,34 @@
+#choose random pixels and fade them to a random color in random time
+#
+
+import time
+import random
+import lib_sl
+
+matrix = lib_sl.matrix(8,6)
+
+def rcolor():
+  return int(random.random()*255)
+
+while "True":
+  chosen = []
+  i=0
+  while ( i < 10):
+   chosen.append(random.choice(matrix))
+   i=i+1
+  r=rcolor()
+  b=rcolor()
+  g=rcolor()
+  t=1+(random.random()*5)
+#  print chosen
+#  print r, b, g, t
+  for p in chosen:
+     lib_sl.send( int(p[0]), int(p[1]), r, g, b, t)
+     time.sleep(t/10) 
+  time.sleep(t/10)
+  for p in chosen:
+     lib_sl.send( int(p[0]), int(p[1]), 0, 0, 0, t)
+     time.sleep(t/10)
+  time.sleep(t/10)
+
+
