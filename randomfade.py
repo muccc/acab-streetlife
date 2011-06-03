f70ba52563f541df1253fe0dd0b2490c2ea64cce
Kilian Neuner <kili@arkadin.club.muc.ccc.de>
2011-06-03 21:33:20 +0200

    clean up randomfade.py

--- randomfade.py
+++ randomfade.py
@@ -1,39 +1,14 @@
-import urllib2
+#choose random pixels and fade them to a random color in random time
+#
+
 import time
-import socket
 import random
+import lib_sl
 
-matrix = []
-
-def gen_matrix():
-    y = 0
-    x = 0
-    while ( y <= 5 ):
-        while (x <=7 ):
-            p =  str(x) + str(y)
-            matrix.append(p)
-            x = x + 1
-            
-        y = y + 1
-        x = 0
+matrix = lib_sl.matrix(8,6)
 
 def rcolor():
   return int(random.random()*255)
-
-sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
-
-def socksend(payload, IP="127.1", PORT=5005):
-  sock.sendto(payload, (IP, PORT))
-        
-gen_matrix()
-
-#IP="127.1"
-#PORT=5005
-
-sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
-
-
-
 
 while "True":
   chosen = []
@@ -44,13 +19,12 @@ while "True":
   r=rcolor()
   b=rcolor()
   g=rcolor()
-  t=2+(random.random()*5)
-  print chosen
-  print r, b, g, t
+  t=1+(random.random()*5)
+#  print chosen
+#  print r, b, g, t
   for p in chosen:
-     m = "/%i/%i/%i/%i/%i/%i" % (int(p[0]), int(p[1]), r, b, g, t*1000)
-     f = urllib2.urlopen("http://127.1:8080"+m)
+     lib_sl.send( int(p[0]), int(p[1]), r, b, g, t)
      time.sleep(t/10) 
-  time.sleep(.5)
+  time.sleep(t/10)
 
 
