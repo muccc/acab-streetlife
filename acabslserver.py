d80bc59be33d929ead70e0cea7bf882cc2ccfa28
schneider <schneider@blinkenlichts.net>
2011-06-03 23:17:53 +0200

    stream to me!

--- acabslserver.py
+++ acabslserver.py
@@ -1,7 +1,7 @@
 import acabsl
 import socket
 
-UDP_IP="127.0.0.1"
+UDP_IP="0.0.0.0"
 UDP_PORT=5005
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
--- lib_sl.py
+++ lib_sl.py
@@ -4,15 +4,21 @@ import socket
 
 HOST="127.1"
 PORT=8080
+
+UDPHOST="83.133.179.35"
 UDPPORT=5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 def send(x,y,r,g,b,t=0):
-  message = "/%s/%s/%i/%i/%i/%i" % (x, y, r, g, b, t*1000)
-  urllib2.urlopen("http://%s:%i"% (HOST, PORT)+message)
+  try:
+    message = "/%s/%s/%i/%i/%i/%i" % (x, y, r, g, b, t*1000)
+    urllib2.urlopen("http://%s:%i"% (HOST, PORT)+message)
+  except:
+    pass
+
   ms = int(t * 1000)
   msg = "%c%c%c%c%c%c%c"%(x,y,r,g,b,ms>>8,ms&0xFF)
-  sock.sendto(msg, (HOST, UDPPORT))
+  sock.sendto(msg, (UDPHOST, UDPPORT))
 
 def matrix(targetsize_x=8,targetsize_y=6):
   x=0
