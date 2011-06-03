8d97f9d719788ea2f3fa149b22e7f0e2cfb4443c
schneider <schneider@blinkenlichts.net>
2011-06-03 23:39:21 +0200

    udp server: keep a queue with maximimum 100 fades

--- acabslserver.py
+++ acabslserver.py
@@ -1,20 +1,35 @@
 import acabsl
 import socket
+import thread
+import time
+import Queue
 
 UDP_IP="0.0.0.0"
 UDP_PORT=5005
+q = Queue.Queue(100)
+
+def writer():
+    while 1:
+        data = q.get()
+        x = ord(data[0])
+        y = ord(data[1])
+        r = ord(data[2])
+        g = ord(data[3])
+        b = ord(data[4])
+        msh = ord(data[5])
+        msl = ord(data[6])
+        ms = (msh<<8) + msl;
+        acabsl.send(x,y,r,g,b,ms/1000.)
+        time.sleep(.001)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP,UDP_PORT))
+thread.start_new_thread(writer,())
 
 while True:
     data = sock.recv(1024)
-    x = ord(data[0])
-    y = ord(data[1])
-    r = ord(data[2])
-    g = ord(data[3])
-    b = ord(data[4])
-    msh = ord(data[5])
-    msl = ord(data[6])
-    ms = (msh<<8) + msl;
-    acabsl.send(x,y,r,g,b,ms/1000.)
+    if not q.full():
+        q.put(data)
+    else:
+        print 'ignoring data'
+
