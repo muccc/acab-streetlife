95b16764d0f9ac8fc9bf389f271009e3b938d5d8
Kilian Neuner <kili@arkadin.club.muc.ccc.de>
2011-06-04 00:25:24 +0200

    add sample playlist.py

--- playlist.py
+++ playlist.py
@@ -1,3 +1,5 @@
+
 playlist = [[5, ["rainbowwall.py", "randomfade.py"]],
             [5, ["crazy.py"]]
+            #[time_in_seconds, [simultaniois_animation1, simultanious_animation2, ....]]
              ]
