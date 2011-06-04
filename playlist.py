efb4d8fddf0cb75822cf55de6adbbe044491659b
Kilian Neuner <kili@arkadin.club.muc.ccc.de>
2011-06-04 18:53:01 +0200

    more entries

--- playlist.py
+++ playlist.py
@@ -14,14 +14,20 @@ playlist = [
 	    [5, ["rainbowwall.py", "randomfade.py"]],
             [5, ["crazy.py"]] ,
             [10, ["kiu-arrows.py"]],
+            [10, ["lines.py", "lines,py"]],
             [15, ["kiu-diebar-rwb.py"]],
+	    [15, [ddc_chooser()]],
+	    [15, ["dots,py"]],
+	    [120, ["randomfade2.py"]],
+	    [30, ["rainbowwall2.py"]],
             [40, ["kiu-font-acab.py"]],
+	    [15, [ddc_chooser()]],
             [30, ["kiu-infinity.py"]],
             [10, ["kiu-random_pixels.py"]],
             [15, ["kiu-soft-scroll.py"]],
             [15, ["kiu-diebar-rw.py"]],
             [10, ["kiu-wabern.py"]],
-            [5, [ddc_chooser()]],
+            [10, [ddc_chooser()]],
 
             #[time_in_seconds, [simultaniois_animation1, simultanious_animation2, ....]]
 ]
