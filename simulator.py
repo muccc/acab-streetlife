978b148d8298af4feb618252be1430c69b287aa2
schneider <schneider@blinkenlichts.net>
2012-05-15 22:14:42 +0200

    added fade support to the simulator

--- simulator.py
+++ simulator.py
@@ -6,21 +6,46 @@ import threading
 import bottle
 from bottle import route, run, request
 
-bottle.debug(True)
+#bottle.debug(True)
+
+xsize = 60
+ysize = 40
+
+xpixels = 16
+ypixels = 6
+
+acab = [[[0,0,0,0,0,0,0] for col in range(ypixels)] for row in range(xpixels)]
 
 def handle_events():
+    surf = pygame.Surface((xsize,ysize))
     while 1:
         for event in pygame.event.get():
             if event.type in (QUIT, QUIT):
                 import os
                 os.kill(os.getpid(), 9)
+        for y in range(0,ypixels):
+            for x in range (0,xpixels):
+                current = acab[x][y][0:3]
+                diff    = acab[x][y][3:6]
+                count   = acab[x][y][6]
 
+                new = [max((0,min((current[i] + diff[i],255)))) for i in range(3)]
+                count -= 1
+                if count == 0:
+                    diff = [0,0,0]
+                
+                surf.fill(tuple(map(int, new)))
+                screen.blit(surf, (x*xsize, y*ysize))
+                
+                acab[x][y][0:3] = new
+                acab[x][y][3:6] = diff
+                acab[x][y][6]   = count
         pygame.display.update()
-        pygame.time.delay(100)
+        pygame.time.delay(33)
 
 pygame.init()
 
-screen = pygame.display.set_mode((240, 120))
+screen = pygame.display.set_mode((xpixels*xsize, ypixels*ysize))
 screen.fill((255, 255, 255))
 
 updatethread = threading.Thread(target=handle_events)
@@ -30,14 +55,26 @@ updatethread.start()
 def fade(**kwargs):
     args = dict(map(lambda tp: (tp[0], int(tp[1])),
         kwargs.iteritems()))
-    surf = pygame.Surface((30,20))
     x = args['x']
     y = args['y']
     r = args['r']
     g = args['g']
     b = args['b']
-    surf.fill(tuple(map(int, (r, g, b))))
-    screen.blit(surf, (x*30, y*20))
+    time = args['t']
+    
+    data = acab[x][y]
+    current = data[0:3]
+    target = [r,g,b]
+    diff = [target[i] - current[i] for i in range(3)]
+    steps = time/33
+    if steps == 0:
+        data[0:3] = target
+        data[3:6] = [0,0,0]
+    else:
+        data[3:6] = [ d/float(steps) for d in diff]
+
+    data[6] = steps
+    acab[x][y] = data
     return '(%s/%s) @ (%s, %s, %s) -> %s' % (x, y, r, g, b, 0)
 
 run(host='localhost', port=8080)
