6b122687ea958aa5a64a9bcc0c4a488c9b2a1726
schneider <schneider@blinkenlichts.net>
2012-05-17 13:44:25 +0200

    serialinterface: handle flaky devices

--- serialinterface.py
+++ serialinterface.py
@@ -5,41 +5,89 @@ import time
 
 class SerialInterface:
     def  __init__ ( self, path2device, baudrate, timeout=0):
-        if path2device == '/dev/null':
-            self.dummy = True
-            return
-        self.dummy = False
-        self.ser = serial.Serial(path2device, baudrate)
-        self.ser.flushInput()
-        self.ser.flushOutput()
-        if timeout:
-            self.ser.setTimeout(timeout)
+      self.portopen = False
+      self.dummy = False
 
-    def write(self, data):
+      if path2device == '/dev/null':
+            self.dummy = True
+            self.portopen = True
+
+      while not self.portopen:
+        try:
+            self.ser = serial.Serial(path2device, baudrate)
+            self.path2device = path2device
+            self.baudrate = baudrate
+            self.timeout = timeout+1
+            self.ser.flushInput()
+            self.ser.flushOutput()
+            if timeout:
+                self.ser.setTimeout(timeout+1)
+            self.portopen = True
+            break
+        except serial.SerialException:
+            #print "Exception while opening", path2device
+            pass
+        time.sleep(1)
+      print "Opened", path2device
+    def close(self):
+        try:
+            self.portopen = False
+            self.ser.close()
+        except serial.SerialException:
+            pass
+    def reinit(self):
+        self.close()
+        #print "reopening"
+        while not self.portopen:
+            self.__init__(self.path2device, self.baudrate, self.timeout)
+            time.sleep(1)
+        #print "done"
+
+    def writeMessage(self,message):
+        enc = "\\1" + message.replace('\\','\\\\') + "\\2";
+        #print 'writing %s' % list(enc)
+        try:
+            self.ser.write(enc)
+        except :
+            pass
+            #self.reinit()
+
+
+    def write(self,message):
+        print 'writing', list(message)
         if self.dummy:
             return
-        #print self, "writing", list(data)
-        self.ser.write(data)
+        try:
+            self.ser.write(message)
+        except :
+            self.reinit()
 
     def readMessage(self):
         data = ""
         escaped = False
         stop = False
         start = False
+        inframe = False
 
         while True:
+            starttime = time.time()
             c = self.ser.read(1)
+            endtime = time.time()
             if len(c) == 0:             #A timout occured
-                print 'TIMEOUT'
-                return False
-            #print "c=", c,list(c)
-        #    continue
+                if endtime-starttime < self.timeout - 1:
+                    print "port broken"
+                    self.reinit()
+                    raise Exception()
+                else:
+                    #print 'TIMEOUT'
+                    return False
             if escaped:
-                if c == '5':
+                if c == '1':
                     start = True
-                    data = ""
+                    inframe = True
                 elif c == '2':
                     stop = True
+                    inframe = False
                 elif c == '\\':
                     d = '\\'
                 escaped = False
@@ -48,16 +96,19 @@ class SerialInterface:
             else:
                 d = c
                 
-            if start:
+            if start and inframe:
                 start = False
             elif stop:
                 if data[0] == 'D':
-                    #print 'serial debug message: %d %s'%(len(data), data)
+                    message = '%f %s'%(time.time(), data[2:])
+                    print 'serial debug message:',data
+                    #print message
                     data = ""
                     stop = False
                 else:
                     #print 'received message: len=%d data=%s'%(len(data),data)
-                    #print list(data)
+                    #print 'received message:',list(data)
                     return data
-            elif escaped == False:
+            elif escaped == False and inframe:
                 data += str(d)
+
