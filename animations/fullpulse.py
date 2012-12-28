5cef7adc83bdf2493b856c2fac071e54e7c2a4da
schneider <schneider@blinkenlichts.net>
2012-12-28 20:36:22 +0100

    full pusle animation

---
+++ animations/fullpulse.py
@@ -0,0 +1,33 @@
+import acabsl
+import colorsys
+import random
+import time
+
+NOOFWALLS=2
+
+tick = 0.5
+
+def set(r,g,b,time):
+    for x in  range(acabsl.WALLSIZEX):
+        for y in range(acabsl.WALLSIZEY):
+            for wall in range(acabsl.NOOFWALLS):
+                acabsl.send(wall,x,y,r,g,b,time)
+h = 0
+col = 0
+acabsl.update()
+while 1:
+    h += random.gauss(0.02,0.05)
+    h = h % 1.
+
+    col += 1
+    col = col % 8
+
+    r,g,b = colorsys.hsv_to_rgb(h, 1., 1.)
+    set(r*255, g*255, b*255, tick)
+    time.sleep(tick*3)
+
+    set(0, 0, 0, tick*3)
+    time.sleep(tick*3)
+
+    
+
