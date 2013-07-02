c9d23b481db698b9e817a2a7aec24993fb4ddd6b
Tobias Schneider <schneider@blinkenlichts.net>
2013-07-02 18:51:52 +0200

    added enerystar script

---
+++ animations/enerystar.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import acabsl
+import time
+import sys
+import schedule
+
+turned_off = False
+
+t = 1
+
+acabsl.update()
+
+def set_all(r,g,b):
+    for w in range(acabsl.NOOFWALLS):
+        for x in range(acabsl.WALLSIZEX):
+            for y in range(acabsl.WALLSIZEY):
+                acabsl.send(x,y,r,g,b,0,w)
+    acabsl.update()
+
+def turn_on():
+    global turned_off
+    turned_off = False
+
+def turn_off():
+    global turned_off
+    turned_off = True
+
+schedule.every().day.at("18:30").do(turn_on)
+schedule.every().day.at("6:00").do(turn_off)
+
+while 1:
+    schedule.run_pending()
+    if turned_off:
+        set_all(0,0,0)
+    time.sleep(t)
+
+
+
