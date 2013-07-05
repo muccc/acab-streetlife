aff9ed773028d7a18fad66f38ea59fa95f2c3f30
acab <acab@acab2.(none)>
2013-07-05 15:28:20 +0200

    updated scheduler for sigint13

--- playlist.py
+++ playlist.py
@@ -19,7 +19,7 @@ def getPlaylist():
             [20, [["python","animations/multiwall_rainbowscroll.py"]]],
             [20, [["python","animations/s-color4-faster.py"]]],
             [20, [["python","animations/gameoflife.py"]]],
-            [20, [["python","animations/dmm/hackerbrucke.py"]]],
+#            [20, [["python","animations/dmm/hackerbrucke.py"]]],
             #[20, [["python","animations/dmm/dt.py"]]],
 #            [20, [["python","animations/dmm/uhrzeit.py"]]],
             [20, [["python","animations/multiwall_warp_core.py"]]],
