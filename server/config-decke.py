323e557a44f97848b608af56ac7706901038784e
Sec <sec@42.org>
2018-01-09 21:55:12 +0000

    post-34c3 muCCC wall config

--- server/config-decke.py
+++ server/config-decke.py
@@ -1,24 +1,19 @@
 server_ip = '0.0.0.0'
 server_port = 7000
-serials = ['/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A10044Xt-if00-port0', '/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A10044Xu-if00-port0']
+serials = ['/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A10044Xp-if00-port0', '/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A400fvDr-if00-port0']
 interfaces = [0, 1]
-matrix = [[(0x57,0), (0x50,1)],
-          [(0x14,0), (0x53,1)],
-          [(0x10,0), (0x43,1)],
-          [(0x6B,0), (0x66,1)],
-          [(0x34,0), (0x23,1)],
-          [(0x4F,0), (0x25,1)],
-          [(0x71,0), (0x29,1)],
-          [(0x22,0), (0x47,1)],
-          [(0x31,0), (0x30,1)],
-          [(0x32,0), (0x46,1)],
-          [(0x3E,0), (0x19,1)],
-          [(0x45,0), (0x24,1)]]
-
-#matrix = [[(0x57,0), (0x50,1)],
-#          [(0x14,0), (0x53,1)],
-#          [(0x6B,0), (0x66,1)],
-#          [(0x6B,0), (0x66,1)]]
+matrix = [[(0x57,0), (0x4A,1)],
+          [(0x6D,0), (0x43,1)],
+          [(0x17,0), (0x41,1)],
+          [(0x42,0), (0x19,1)],
+          [(0x67,0), (0x38,1)],
+          [(0x20,0), (0x56,1)],
+          [(0x5A,0), (0x3E,1)],
+          [(0x5C,0), (0x4C,1)],
+          [(0x4E,0), (0x3F,1)],
+          [(0x22,0), (0x6B,1)],
+          [(0x28,0), (0x47,1)],
+          [(0x26,0), (0x45,1)]]
 
 simulation = False
 router_base_port = 8000
